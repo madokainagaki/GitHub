b83684657466258555513fcd98920587665a4f0a
madokainagaki <jo6gencho9@gmail.com>
2022-06-23 09:38:30 +0900

    git branch issue1

--- app/src/main/java/com/example/github/MainActivity.kt
+++ app/src/main/java/com/example/github/MainActivity.kt
@@ -7,5 +7,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // git branch <branchname>
+            // ブランチの作成
+            // git branch issue1 で作ってみる
+            // issue → 問題点・論点とかいう意味
     }
 }
